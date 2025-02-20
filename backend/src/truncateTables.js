f720e794807d09b2e399fa50c5dea69d562d287e
Akhil-Patil-Bagili <akhilpatilbagili@gmail.com>
2025-02-19 20:19:41 -0800

    added documentation

--- backend/src/truncateTables.js
+++ backend/src/truncateTables.js
@@ -3,7 +3,9 @@ const prisma = require('./prismaClient');
 async function truncateTables() {
   await prisma.$executeRaw`TRUNCATE TABLE "Match" RESTART IDENTITY CASCADE;`;
   await prisma.$executeRaw`TRUNCATE TABLE "MatchBetmate" RESTART IDENTITY CASCADE;`;
-  // await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE;`;
+  await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE;`;
+  await prisma.$executeRaw`TRUNCATE TABLE "FriendRequest" RESTART IDENTITY CASCADE;`;
+  await prisma.$executeRaw`TRUNCATE TABLE "PastBetmate" RESTART IDENTITY CASCADE;`;
   console.log('Tables truncated');
   process.exit(0);
 }
